3e4c3703df8a2d4620989bbf226ccbc6185d250a
Robert LaCagnina <robertlacagnina@Roberts-MacBook-Pro.local>
2024-09-16 23:57:52 -0400

    directory methods, list dir contents

--- directory.ts
+++ directory.ts
@@ -6,4 +6,24 @@ export class Directory {
         this.name = name;
         this.children = new Map();
     }
+
+    addChild(directoryName) {
+        this.children.set(directoryName, new Directory(directoryName));
+    }
+
+    deleteChild(directoryName) {
+        const deleted = this.children.delete(directoryName);
+
+        if (!deleted) {
+            throw new Error(`${directoryName} does not exist`);
+        }
+    }
+
+    listContents(level: number = 0) {
+        const indenation = ' '.repeat(level);
+        this.children.forEach((child) => {
+            console.log(`${indenation}${child.name}`);
+            child.listContents(level++);
+        });
+    }
 }
